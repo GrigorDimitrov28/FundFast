73cdbf6d2c104e9fd44968948682f14d0998299f
GrigorDimitrov28 <g.dimitroff28@gmail.com>
2020-07-26 21:29:57 +0300

    feat: add front-end validation

--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -39,16 +39,16 @@ const Featured = () => {
         <div className={styles.featured}>
             <div className={styles.buttons}>
                 <h2>Featured</h2>
-                <button className={current == 'Donations' ? styles.active : styles.button}
+                <button className={current === 'Donations' ? styles.active : styles.button}
                  onClick={(e) => setCurrent('Donations')}>Donations</button>
 
-                <button className={current == 'Campaigns' ? styles.active : styles.button}
+                <button className={current === 'Campaigns' ? styles.active : styles.button}
                  onClick={(e) => setCurrent('Campaigns')}>Campaigns</button>
 
-                <button className={current == 'Product development' ? styles.active : styles.button}
+                <button className={current === 'Product development' ? styles.active : styles.button}
                  onClick={(e) => setCurrent('Product development')}>Product development</button>
 
-                <button className={current == 'Startups' ? styles.active : styles.button}
+                <button className={current === 'Startups' ? styles.active : styles.button}
                  onClick={(e) => setCurrent('Startups')}>Startups</button>
             </div>
 
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -8,6 +8,66 @@ import { Link } from 'react-router-dom'
 class LoginForm extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            username: "",
+            password: "",
+            rePassword: "",
+            usernameErrorMsg: "",
+            passwordErrorMsg: "",
+            usernameErrorIsHidden: true,
+            passwordErrorIsHidden: true
+        }
+    }
+
+    handleBlurUsername = () => {
+        const usernameRegex = new RegExp("^[a-zA-Z\\d]+$", "g")
+
+        if (this.state.username.length < 4) {
+            this.setState({ usernameErrorIsHidden: false, usernameErrorMsg: `Username must be at least 4 characters long.` })
+        } else if (this.state.username.length > 15) {
+            this.setState({ usernameErrorIsHidden: false, usernameErrorMsg: `Username must be maximum 15 characters long.` })
+        } else if (!usernameRegex.test(this.state.username)) {
+            this.setState({ usernameErrorIsHidden: false, usernameErrorMsg: `Username can contain only letters and digits.` })
+        } else {
+            this.setState({ usernameErrorIsHidden: true })
+        }
+
+    }
+
+    handleBlurPassword = () => {
+        const lowerCaseRegex = new RegExp("[a-z]+", "g")
+        const upperCaseRegex = new RegExp("[A-Z]+", "g")
+        const numberRegex = new RegExp("[0-9]+", "g")
+        const specialCharRegex = new RegExp("[@$!%*?&]+", "g")
+
+        if (this.state.password.length < 8) {
+            this.setState({ passwordErrorIsHidden: false, passwordErrorMsg: `Password must be at least 8 characters long.` })
+        } else if (this.state.password.length > 20) {
+            this.setState({ passwordErrorIsHidden: false, passwordErrorMsg: `Password must be maximum 20 characters long.` })
+        } else if (!specialCharRegex.test(this.state.password)) {
+            this.setState({ passwordErrorIsHidden: false, passwordErrorMsg: `Password must contain one of these (@, $, !, %, *, ?, &).` })
+        } else if (!numberRegex.test(this.state.password)) {
+            this.setState({ passwordErrorIsHidden: false, passwordErrorMsg: `Password must contain at least one digit.` })
+        } else if (!upperCaseRegex.test(this.state.password)) {
+            this.setState({ passwordErrorIsHidden: false, passwordErrorMsg: `Password must contain at least one upper case letter.` })
+        } else if (!lowerCaseRegex.test(this.state.password)) {
+            this.setState({ passwordErrorIsHidden: false, passwordErrorMsg: `Password must contain at least one lower case letter.` })
+        } else {
+            this.setState({ passwordErrorIsHidden: true })
+        }
+    }
+
+    handleChange = (e, type) => {
+        const usernameRegex = new RegExp("^[a-zA-Z\\d]+$", "g")
+        const passwordRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,20}$", "g")
+
+        this.setState({ [`${type}`]: e.target.value })
+        if(type == "username" && usernameRegex.test(e.target.value)){
+            this.setState({usernameErrorIsHidden: true})
+        }else if(type == "password" && passwordRegex.test(e.target.value)){
+            this.setState({passwordErrorIsHidden: true})
+        }
     }
 
     render() {
@@ -16,10 +76,23 @@ class LoginForm extends Component {
                 <div className={styles.loginWrapper}>
                     <h2 className={styles.login}>Sign In</h2>
                     <form className={styles.loginForm}>
-                        <Input name="email" placeholder="Email" />
-                        <Input name="password" placeholder="Password" />
-                        <button type="submit" className={styles.submit}>
-                            Sign In
+                        <Input name="username"
+                            placeholder="Username"
+                            value={this.state.username}
+                            onBlur={this.handleBlurUsername}
+                            onChange={(e) => this.handleChange(e, 'username')} />
+                        <p className={styles.error} hidden={this.state.usernameErrorIsHidden}> {this.state.usernameErrorMsg} </p>
+
+                        <Input name="password"
+                            placeholder="Password"
+                            value={this.state.password}
+                            onBlur={this.handleBlurPassword}
+                            onChange={(e) => this.handleChange(e, 'password')} />
+                        <p className={styles.error} hidden={this.state.passwordErrorIsHidden}> {this.state.passwordErrorMsg} </p>
+
+                        <button type="submit"
+                            disabled={this.state.usernameErrorIsHidden === false || this.state.passwordErrorIsHidden === false} className={styles.submit}>
+                            Sign Up
                         </button>
                     </form>
                     <div className={styles.or}>
@@ -35,7 +108,7 @@ class LoginForm extends Component {
                     </button>
                     <div className={styles.new}>
                         <p>New to FundFast?</p>
-                        <Link className={styles.register}>Sign Up</Link>
+                        <Link to={'/register'} className={styles.register}>Sign Up</Link>
                     </div>
                 </div>
             </div>
