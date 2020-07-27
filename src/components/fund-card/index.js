983a80dc0b37b64ac64e612d941f0fb289bece34
GrigorDimitrov28 <g.dimitroff28@gmail.com>
2020-07-27 23:10:29 +0300

    fix: styles

--- src/components/fund-card/index.js
+++ src/components/fund-card/index.js
@@ -5,8 +5,10 @@ const Card = () => {
         <div className={styles.card}>
             <img src="https://image.shutterstock.com/image-photo/highquality-headphones-on-white-background-600w-1574611990.jpg" alt="Avatar"/>
                 <div className={styles.container}>
-                    <h4><b>John Doe</b></h4>
-                    <p>Architect & Engineer</p>
+                    <h3><b>John Doe</b></h3>
+                    <h4>Architect & Engineer</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean luctus pretium velit, quis varius neque. Proin elit nunc, euismod sit.</p>
+                    <a href="/details">Details</a>
                 </div>
         </div>
     )
--- src/components/img-under-nav/index.js
+++ src/components/img-under-nav/index.js
@@ -4,7 +4,8 @@ import styles from './index.module.css';
 const Photo = ({ source }) => {
     return (
         <div className={styles.photo} id="photo-holder">
-            <img className={styles['big-photo']} src="big-image-2.jpg" alt="not working" />
+            <img className={styles['big-photo']} 
+            src="https://images.unsplash.com/photo-1578091436046-ecd3f4fe6992?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" />
         </div>
     )
 }
