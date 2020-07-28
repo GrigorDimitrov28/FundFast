a280955e5f6ba56c33d004353753c26b0a26ca18
GrigorDimitrov28 <g.dimitroff28@gmail.com>
2020-07-28 16:54:36 +0300

    fix: header photo

--- src/components/img-under-nav/index.js
+++ src/components/img-under-nav/index.js
@@ -3,10 +3,10 @@ import styles from './index.module.css';
 
 const Photo = ({ source }) => {
     return (
-        <div className={styles.photo} id="photo-holder">
-            <img className={styles['big-photo']} 
-            src="https://images.unsplash.com/photo-1578091436046-ecd3f4fe6992?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" />
-        </div>
+
+        <img className={styles['big-photo']}
+            src="./header-photo.png" />
+
     )
 }
 
