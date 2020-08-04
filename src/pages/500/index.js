80a8856159de65cc3c4e7f12d24b710a0536b7b7
GrigorDimitrov28 <g.dimitroff28@gmail.com>
2020-08-04 14:18:48 +0300

    feat: add error 500 page

--- src/pages/500/index.js
+++ src/pages/500/index.js
@@ -1,16 +1,35 @@
 import React from 'react'
 import Navbar from '../../components/navigation'
 import Footer from '../../components/footer'
-import styles from './index.module.css'
-const LazyServerErrorPage = () => {
+import { useHistory } from 'react-router-dom'
+import styles from './500.css'
+
+const Content = () => {
+    const history = useHistory()
+
+    const handleClick = () => {
+        history.goBack()
+    }
+
     return (
         <div >
-            <Navbar />
-            <div className={styles.wrapper}>
-                <img className={styles.error} src="./500png.png" alt="error 500" />
+            <div id="container-500">
+                <img id="left-500" src="./error.png" alt="error" />
+                <h1 id="h1-500">500</h1>
+                <img id="right-500" src="./error.png" alt="error" />
             </div>
+            <h2 id="h2-500">Internal server error <b>:(</b></h2>
+            <button id="button-500" onClick={handleClick}>Try again.</button>
+        </div>
+    )
+}
 
-            <Footer />
+const LazyServerErrorPage = () => {
+    return (
+        <div>
+            <Navbar />
+            <Content />
+            <Footer type={"sticky"}/>
         </div>
     )
 }
